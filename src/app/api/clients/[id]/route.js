d44aa725c9dee748eeea080be71cf63faf3dac21
João <joao.codelearning@gmail.com>
2025-09-13 18:21:42 -0300

    ADS: 01748208 REST-cli

---
+++ src/app/api/clients/[id]/route.js
@@ -0,0 +1,153 @@
+/**
+ * Nome do arquivo: [id]/route.js
+ * Data de criação: 13/09/2025
+ * Autor: João Pompeu
+ * Matrícula: 01748208
+ *
+ * Descrição:
+ * 
+ * Criação do endpoint dinâmico '/clients/[id]' com suas rotas:
+ * GET /clients/[id]: busca e retorna o cliente correspondente ao ID
+ * PUT /clients/[id]: busca e atualiza o cliente indicado pelo ID
+ * DELETE /clients/[id]: busca e deleta o cliente indicado pelo ID
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+import { prisma } from "../../../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+
+// método GET para buscar um cliente específico
+export async function GET(request, { params }) {
+  // desestruturando params
+  const { id } = params;
+
+  try {
+    // converte o id para inteiro
+    const clientID = parseInt(id);
+
+    // verifica se o ID é um número válido
+    if (isNaN(clientID))
+      return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+
+    // caso o ID seja válido, procura o ID no banco e retorna o cliente específico
+    const client = await prisma.client.findUnique({
+      where: { id: clientID },
+    });
+
+    // caso o ID não corresponda a nenhum cliente cadastrado, retorna erro 404 not found
+    if (!client)
+      return NextResponse.json(
+        { error: "Cliente não encontrado." },
+        { status: 404 }
+      );
+
+    return NextResponse.json(client, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao localizar cliente" },
+      { status: 500 }
+    );
+  }
+}
+
+// método PUT para atualizar dados do cliente 
+export async function PUT(request, { params }) {
+  try {
+    const body = await request.json();
+    // desestruturação do body e params
+    const { name, address, phone, email } = body;
+    const { id } = params;
+    // converte o id para inteiro
+    const clientID = parseInt(id);
+
+    // verifica se o ID é um número válido
+    if (isNaN(clientID))
+      return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+
+    // valida os campos individualmente
+    if (!name || !address || !phone || !email) {
+      return NextResponse.json(
+        { error: "Todos os campos devem ser preenchidos." },
+        { status: 400 }
+      );
+    }
+
+    // após a validação, atualiza os dados do cliente
+    const updatedClient = await prisma.client.update({
+      where: { id: clientID },
+      data: {
+        name,
+        address,
+        phone,
+        email,
+      },
+    });
+
+    return NextResponse.json(updatedClient, { status: 200 });
+  } catch (error) {
+
+    // error.code fornecido pelo prisma para especificar o erro
+    if (error.code === "P2025") {
+      return NextResponse.json(
+        { error: "Cliente não encontrado" },
+        { status: 404 }
+      );
+    }
+
+    // error.code fornecido pelo prisma para especificar o erro
+    if (error.code === "P2002") {
+      return NextResponse.json(
+        { error: "Email já está em uso por outro cliente." },
+        { status: 409 }
+      );
+    }
+
+    // erro genérico
+    return NextResponse.json(
+      { error: "Erro ao atualizar cliente." },
+      { status: 500 }
+    );
+  }
+}
+
+// método DELETE para deletar cliente
+export async function DELETE(request,{ params }) {
+  // desestruturando params
+  const { id } = params;
+
+  try {
+    // convertendo o id para inteiro
+    const clientID = parseInt(id);
+
+    // validando o id
+    if (isNaN(clientID))
+      return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+
+    // deletando cliente
+    await prisma.client.delete({
+      where: { id: clientID },
+    });
+
+
+    return NextResponse.json(
+      { message: "Cliente deletado com sucesso." },
+      { status: 200 }
+    );
+  } catch (error) {
+
+    // error.code fornecido pelo prisma para especificar o erro
+    if (error.code === "P2025") {
+      return NextResponse.json(
+        { error: "Cliente não encontrado" },
+        { status: 404 }
+      );
+    }
+    // erro genérico
+    return NextResponse.json(
+      { error: "Erro ao deletar cliente" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/clients/route.js
@@ -0,0 +1,65 @@
+/**
+ * Nome do arquivo: clients/route.js
+ * Data de criação: 13/09/2025
+ * Autor: João Pompeu
+ * Matrícula: 01748208
+ *
+ * Descrição:
+ * 
+ * Criação do endpoint '/clients' com suas rotas:
+ * GET /clients: busca e retorna todos os clientes cadastrados
+ * POST /clients: cadastra um novo cliente no banco
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+import { prisma } from "../../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+// método GET para buscar todos os clientes
+export async function GET() {
+  try {
+    const clients = await prisma.client.findMany();
+    return NextResponse.json(clients, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao requisitar dados." },
+      { status: 500 }
+    );
+  }
+}
+
+
+// método POST para criar novo cliente no banco
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    // Desestruturando o body para validação dos campos individualmente
+    const { name, address, phone, email } = body;
+
+    // Validando os campos
+    if (!name || !address || !phone || !email) {
+      return NextResponse.json(
+        { error: "Todos os campos devem ser preenchidos." },
+        { status: 400 }
+      );
+    }
+
+    // Se passar da validação, cria novo cliente no banco
+    const newClient = await prisma.client.create({
+      data: {
+        name: body.name,
+        address: body.address,
+        phone: body.phone,
+        email: body.email,
+      },
+    });
+
+    return NextResponse.json(newClient, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao cadastrar novo cliente." },
+      { status: 500 }
+    );
+  }
+}
