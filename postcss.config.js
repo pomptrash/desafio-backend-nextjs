d3187ee2062ce2b7f40e5023ee0cd94b055cc6aa
João <joao.codelearning@gmail.com>
2025-09-23 19:51:25 -0300

    ADS: 01748208 /+ configuração inicial do tailwind.css

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+/**
+ * Nome do arquivo: postcss.config.js
+ * Data de criação: 23/09/2025
+ * Autor: João Pompeu
+ * Matrícula: 01748208
+ *
+ * Descrição:
+ * arquivo css responsável por configuração inicial do tailwind.css 
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/**
+ * Nome do arquivo: tailwind.config.js
+ * Data de criação: 23/09/2025
+ * Autor: João Pompeu
+ * Matrícula: 01748208
+ *
+ * Descrição:
+ * arquivo css responsável por configuração inicial do tailwind.css 
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
